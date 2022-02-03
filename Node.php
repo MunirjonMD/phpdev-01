e21f0689ae80bcbf1e74715975daea2a2b5d3c04
Munir <dodojonovmunirjon@gmail.com>
2022-02-04 00:47:58 +0300

    home work fine test

--- Node.php
+++ Node.php
@@ -1,72 +1,81 @@
 <?php
 
-include_once ('./NodeInterface.php');
+include_once('./NodeInterface.php');
 
 class Node implements NodeInterface
 {
-    
-	/**
-	 *
-	 * @param string $name 
-	 *
-	 * @return mixed
-	 */
+
+    /**
+     *
+     * @param string $name
+     *
+     * @return mixed
+     */
     private $name;
-	private $children;
-	public function __construct(string $name) {
-        $this->name=$name;
-        $this->children=[];
-	}
-	
-	/**
-	 *
-	 * @return string
-	 */
-	public function __toString(): string {
-        return $this->prepareTree($this,"","+");
-	}
-	
-    private function prepareTree(Node $node,$str,$startPlus) 
-    {        
-        $str.=$startPlus.$node->getName()."\n\t";
-        if($node->hasChild()){
-            foreach($node->getChildren() as $nodeChild){
-                $this->prepareTree($nodeChild,$str,$startPlus."+");
-            }    
-        }
-        return $str;
+    private $children;
+    private $tree = "";
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+        $this->children = [];
     }
 
-	/**
-	 *
-	 * @return string
-	 */
-	public function getName(): string {
-        return $this->name;
-	}
-	
-	/**
-	 *
-	 * @return array
-	 */
-	public function getChildren(): array {
-        return $this->children;
-	}
-	public function hasChild():bool
+    /**
+     *
+     * @return string
+     */
+    public function __toString(): string
     {
-		print_r($this->children); 
-        if(!empty($this->children)) return true;
+        $this->prepareTree($this, "+");
+        return $this->tree;
+    }
+
+    private function prepareTree(Node $node, $startPlus)
+    {
+        $this->tree .= $startPlus . $node->getName() . "<br>";
+        if ($node->hasChild()) {
+            foreach ($node->getChildren() as $nodeChild) {
+                $this->prepareTree($nodeChild, $startPlus . "+");
+            }
+        }
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     *
+     * @return array
+     */
+    public function getChildren(): array
+    {
+        return $this->children;
+    }
+
+    public function hasChild(): bool
+    {
+        if (!empty($this->children)) return true;
         return false;
     }
-	/**
-	 *
-	 * @param Node $node 
-	 *
-	 * @return NodeInterface
-	 */
-	public function addChild(Node $node): NodeInterface {
-		array_push($this->children,$node);
+
+    /**
+     *
+     * @param Node $node
+     *
+     * @return NodeInterface
+     */
+    public function addChild(Node $node): NodeInterface
+    {
+        array_push($this->children, $node);
         return $this;
-	}
+    }
 }
+
 ?>
